f1d22e863fcf0161c5362af821dc5fd64b20554f
ercansahin16 <esahin.ast@gmail.com>
2024-10-10 01:03:48 +0300

    doc

--- components/data.jsx
+++ components/data.jsx
@@ -1,28 +1,28 @@
 
  export const muzikListesi = [
     { id: 1, ad: "Azimov - Nice To Meet You (Original Mix) - 128", 
-     dosya: "/music/Azimov - Nice To Meet You (Original Mix) - 128.mp3" },
+     dosya: "../public/music/Azimov - Nice To Meet You (Original Mix) - 128.mp3" },
 
     { id: 2, ad: "Emre Fel - Sana El Pençe Durmam (Official Video) - 128", 
-        dosya: "/music/Emre Fel - Sana El Pençe Durmam (Official Video) - 128.mp3" },
+        dosya: "../public/music/Emre Fel - Sana El Pençe Durmam (Official Video) - 128.mp3" },
 
     { id: 3, ad: "ADAM, Elyanna & Massari",
-         dosya: "/music/ADAM, Elyanna & Massari.mp3" },
+         dosya: "../public/music/ADAM, Elyanna & Massari.mp3" },
 
     { id: 4, ad: "Aşık Veysel - Kara Toprak (1969)  TRT Arşiv - 128",
 
-         dosya: "/music/Aşık Veysel - Kara Toprak (1969)  TRT Arşiv - 128.mp3" },
+         dosya: "../public/music/Aşık Veysel - Kara Toprak (1969)  TRT Arşiv - 128.mp3" },
 
     { id: 5, ad: "Mezar Turizm - Hayatı şikayet ediyorum - 128",
-         dosya: "/music/Mezar Turizm - Hayatı şikayet ediyorum - 128.mp3" },
+         dosya: "../public/music/Mezar Turizm - Hayatı şikayet ediyorum - 128.mp3" },
 
     { id: 6, ad: "Murat Göğebakan - Karanfil Kokan Yarim ( Official Audio ) - 128",
-         dosya: "/music/Murat Göğebakan - Karanfil Kokan Yarim ( Official Audio ) - 128.mp3" },
+         dosya: "../public/music/Murat Göğebakan - Karanfil Kokan Yarim ( Official Audio ) - 128.mp3" },
 
     { id: 7, ad: "Murat Göğebakan - Turnalar ( Official Audio ) - 128",
-         dosya: "/music/Murat Göğebakan - Turnalar ( Official Audio ) - 128.mp3" },
+         dosya: "../public/music/Murat Göğebakan - Turnalar ( Official Audio ) - 128.mp3" },
 
     { id: 8, ad: "YILLAR UTANSIN - 128", 
-        dosya: "/music/YILLAR UTANSIN - 128.mp3" },
+        dosya: "../public/music/YILLAR UTANSIN - 128.mp3" },
         
   ];
